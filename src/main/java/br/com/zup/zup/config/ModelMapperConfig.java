f33144e4daf8c79f3142c8ce6a4c599f9390d237
Weslley Lima <weslley.lima@zup.com.br>
2021-04-07 16:43:21 -0300

    Criar estrutura do Projeto

---
+++ src/main/java/br/com/zup/zup/config/ModelMapperConfig.java
@@ -0,0 +1,19 @@
+package br.com.zup.zup.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper;
+    }
+
+}
---
+++ src/test/java/br/com/zup/zup/DesafioM5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.zup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioM5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
